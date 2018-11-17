14d99bde2007b6a57de2c3ec30a8b9c96e81b6f3
Laurence Nairne <laurencenairne@gmail.com>
2018-11-17 21:49:06 +0000

    Added in model validation for fields - not perfect, but better

--- aspnet_thirdpartyapi/Models/WeatherEditModel.cs
+++ aspnet_thirdpartyapi/Models/WeatherEditModel.cs
@@ -5,12 +5,15 @@ namespace aspnet_thirdpartyapi.Models
 {
     public class WeatherEditModel
     {
+        [RegularExpression(@"^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$")]
         [Required]
         public double Latitude { get; set; }
 
+        [RegularExpression(@"^[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$")]
         [Required]
         public double Longitude { get; set; }
 
+        [Range(1000000000, 9999999999)]
         [Required]
         public long Time { get; set; }
     }
