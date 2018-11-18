72b5cc75f35ed42f56dfc5eb718e89c44259cb73
Laurence Nairne <laurencenairne@gmail.com>
2018-11-18 21:06:05 +0000

    Cleaned up using directives

--- aspnet_thirdpartyapi/Services/IGetWeather.cs
+++ aspnet_thirdpartyapi/Services/IGetWeather.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using aspnet_thirdpartyapi.Models;
 
 namespace aspnet_thirdpartyapi.Services
