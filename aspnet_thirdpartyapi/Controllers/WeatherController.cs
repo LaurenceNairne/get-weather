dfce25938adf26583821bde30b7b33a925594e74
Laurence Nairne <laurencenairne@gmail.com>
2018-11-18 21:07:05 +0000

    Cleaned up using directives

--- aspnet_thirdpartyapi/Controllers/WeatherController.cs
+++ aspnet_thirdpartyapi/Controllers/WeatherController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using aspnet_thirdpartyapi.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace aspnet_thirdpartyapi.Controllers
