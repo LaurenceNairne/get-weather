b913599c976072cf0ee10f6715a64cdd2888a96e
Laurence Nairne <laurencenairne@gmail.com>
2018-11-20 13:43:15 +0000

    Cleaned up using directives

--- aspnet_thirdpartyapi/Startup.cs
+++ aspnet_thirdpartyapi/Startup.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using aspnet_thirdpartyapi.Services;
+﻿using aspnet_thirdpartyapi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,15 +8,12 @@ namespace aspnet_thirdpartyapi
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddScoped<IGetWeather, GetWeather>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
