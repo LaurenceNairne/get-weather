9aeab82814eaf8660423cab7e1fc6295e6fbaa45
Laurence Nairne <laurencenairne@gmail.com>
2018-11-20 13:42:30 +0000

    Removed required attributes as we're not using this for input

--- aspnet_thirdpartyapi/Models/WeatherModel.cs
+++ aspnet_thirdpartyapi/Models/WeatherModel.cs
@@ -4,12 +4,8 @@ namespace aspnet_thirdpartyapi.Models
 {
     public class WeatherModel
     {
-        [Required]
         public double Latitude { get; set; }
-
-        [Required]
         public double Longitude { get; set; }
-
         public Currently Currently { get; set; }
     }
 }
